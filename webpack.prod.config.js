ac7afbc90159e18f50d7f91f9dbd89b441411474
jadrian_s <jadrian.s@gmail.com>
2025-01-20 05:15:19 -0600

    fix: validacion de curp

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,6 @@
 const { createConfig } = require('@openedx/frontend-build');
 
-const config = createConfig('webpack-dev');
+const config = createConfig('webpack-prod');
 
 config.module.rules[0].exclude = /node_modules\/(?!(fastest-levenshtein|@edx))/;
 

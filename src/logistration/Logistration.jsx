fbf9f042d84b56612b3d9e86be7f5a91ffe2ebfc
LuisAbonce <ltorresa1701@alumno.ipn.mx>
2025-08-13 12:43:28 -0600

    style: replace logo image with new version and enhance layout styling for visual clarity

--- src/logistration/Logistration.jsx
+++ src/logistration/Logistration.jsx
@@ -100,8 +100,8 @@ const Logistration = (props) => {
   const mfeBase = (getConfig().BASE_URL || '').replace(/\/$/, '');
   const lmsBase = (getConfig().LMS_BASE_URL || '').replace(/\/$/, '');
   const logoCandidates = [
-    `${mfeBase}/images/EMI_completo_guinda.png`,
-    `${lmsBase}/theming/asset/images/EMI_completo_guinda.png`,
+    `${mfeBase}/images/EMI_completo_negro.png`,
+    `${lmsBase}/theming/asset/images/EMI_completo_negro.png`,
     `${lmsBase}/theming/asset/images/logo.png`,
   ];
 

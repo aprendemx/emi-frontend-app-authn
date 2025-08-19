5233145cf15f817647fb97ad655c441c0fa156eb
nicolas.1im9.09 <nicolas.1im9.09@gmail.com>
2025-08-19 14:45:20 -0600

    refactor: simplify PhoneField component by removing unused props and optimizing event handling

--- src/register/RegistrationFields/PhoneField/PhoneField.jsx
+++ src/register/RegistrationFields/PhoneField/PhoneField.jsx
@@ -9,44 +9,43 @@ import { FormGroup } from '../../../common-components';
  * - handleErrorChange for setting error
  */
 const PhoneField = (props) => {
-  const {
-    name,
-    value,
-    handleChange,
-    handleErrorChange,
-    errorMessage,
-    floatingLabel,
-    helpText,
-  } = props;
+  const { handleErrorChange, name } = props;
 
   const handleOnFocus = () => {
     handleErrorChange(name, '');
   };
 
-  const handleOnChange = (event) => {
-    // Solo permitir números y limitar a 10 dígitos
-    const phoneValue = event.target.value.replace(/\D/g, '').slice(0, 10);
-    const modifiedEvent = {
-      ...event,
-      target: {
-        ...event.target,
-        value: phoneValue,
-      },
-    };
-    handleChange(modifiedEvent);
+  const handleOnBlur = (event) => {
+    const { value } = event.target;
+    // Limpiar el valor: solo números, máximo 10 dígitos
+    const cleanValue = value.replace(/\D/g, '').slice(0, 10);
+    
+    // Validar longitud
+    if (cleanValue.length > 0 && cleanValue.length < 10) {
+      handleErrorChange(name, 'El número de teléfono debe tener exactamente 10 dígitos');
+    } else if (cleanValue.length === 0) {
+      handleErrorChange(name, 'El número de teléfono es requerido');
+    }
+    
+    // Si el valor cambió, actualizar el campo
+    if (cleanValue !== value) {
+      const syntheticEvent = {
+        target: {
+          name: name,
+          value: cleanValue,
+        },
+      };
+      props.handleChange(syntheticEvent);
+    }
   };
 
   return (
     <FormGroup
-      name={name}
-      value={value}
-      handleChange={handleOnChange}
-      handleFocus={handleOnFocus}
-      errorMessage={errorMessage}
-      helpText={helpText}
-      floatingLabel={floatingLabel}
-      maxLength={10}
+      {...props}
       type="tel"
+      handleBlur={handleOnBlur}
+      handleFocus={handleOnFocus}
+      maxLength={10}
       pattern="[0-9]{10}"
     />
   );
@@ -54,7 +53,6 @@ const PhoneField = (props) => {
 
 PhoneField.defaultProps = {
   errorMessage: '',
-  helpText: [],
 };
 
 PhoneField.propTypes = {
@@ -63,8 +61,6 @@ PhoneField.propTypes = {
   handleChange: PropTypes.func.isRequired,
   handleErrorChange: PropTypes.func.isRequired,
   errorMessage: PropTypes.string,
-  floatingLabel: PropTypes.string.isRequired,
-  helpText: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default PhoneField;

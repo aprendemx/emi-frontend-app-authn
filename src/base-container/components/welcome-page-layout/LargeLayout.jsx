417b77c9f352114eb0e2dad4f6b956b6c406794f
nicolas.1im9.09 <nicolas.1im9.09@gmail.com>
2025-11-26 17:49:40 -0600

    refactor: simplify LargeLayout structure and remove unused elements

--- src/base-container/components/welcome-page-layout/LargeLayout.jsx
+++ src/base-container/components/welcome-page-layout/LargeLayout.jsx
@@ -11,30 +11,16 @@ const LargeLayout = ({ fullName }) => {
   const { formatMessage } = useIntl();
 
   return (
-    <div className="w-50 d-flex">
-      <div className="col-md-10 bg-light-200 p-0">
-        <Hyperlink destination={getConfig().MARKETING_SITE_BASE_URL}>
-          <Image className="logo position-absolute" alt={getConfig().SITE_NAME} src={getConfig().LOGO_URL} />
-        </Hyperlink>
-        <div className="min-vh-100 d-flex align-items-center">
-          <div className="container-llaveMX">
-            <div className="left-section">
-              <div className="boxLogin">
-                <h1>
-                  {formatMessage(messages['welcome.to.platform'], {siteName: getConfig().SITE_NAME, fullName})}
-                </h1>
-                <img src="https://aprende.gob.mx/images/cursos-amx.png" alt="AprendeMX-Logo" />
-              </div>
-            </div>
+    <div className="fondo-llaveMX">
+      <div className="container-llaveMX">
+        <div className="left-section">
+          <div className="boxLogin">
+            <h1>
+              {formatMessage(messages['welcome.to.platform'], {siteName: getConfig().SITE_NAME, fullName})}
+            </h1>
+            <img src="https://aprende.gob.mx/images/cursos-amx.png" alt="AprendeMX-Logo" />
           </div>
         </div>
-      </div>
-      <div className="col-md-2 bg-white p-0">
-        <svg className="m1-n1 w-100 h-100 large-screen-svg-light" preserveAspectRatio="xMaxYMin meet">
-          <g transform="skewX(171.6)">
-            <rect x="0" y="0" height="100%" width="100%" />
-          </g>
-        </svg>
       </div>
     </div>
   );
